a31a8b2df40899829115cc9a8591e7804c7f0021
ip-mba <iptton@gmail.com>
2015-03-26 23:10:22 +0800

    initial

---
+++ minesweeper/minesweeper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  minesweeper
+//
+//  Created by pxz on 15-3-26.
+//  Copyright (c) 2015年 iptton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ minesweeper/minesweeper/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  minesweeper
+//
+
+//  Copyright (c) 2015年 iptton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
